450b35f8c8cf933d45e53adfa6b1fb21abbcfb09
anne <bauer@ieec.uab.es>
2014-03-19 19:10:23 +0100

    wrote a partpix degrade that uses less than 2GB, now i'm going for a drink.

--- scripts/partpix_map.py
+++ scripts/partpix_map.py
@@ -18,6 +18,8 @@ import json
 import os
 import numpy as np
 import tables
+from scipy import weave
+from scipy.weave import converters
 
 class Partpix_Map:
     def __init__(self, o=None, s=0):
@@ -47,10 +49,7 @@ class Partpix_Map:
     
     def __getitem__(self, i):
         indices = np.searchsorted(self.pixel_mapping_arraytohigh,i)
-        print "i = "
-        print i
-        print "indices ="
-        print indices
+        assert self.pixel_mapping_arraytohigh[indices] == i, "partpix_map:__getitem__: all indices not found in the map"
         return self.partmap[indices]
         
     def mean(self):
@@ -163,6 +162,11 @@ class Partpix_Map:
             self.partmap[i] = vs[i]
         self.update()
     
+    def to_healpix(self):
+        healpix_map = np.zeros(self.npix)
+        healpix_map[self.pixel_mapping_arraytohigh] = self.partmap
+        return healpix_map
+        
 def read_from_hdf5file( map_filename, read_map=True, read_mask=True ):
     
     mapfile = tables.openFile(map_filename)
@@ -193,4 +197,190 @@ def read_from_hdf5file( map_filename, read_map=True, read_mask=True ):
         map1.update()
     return map1, mask1
 
+
+code_degrade_rings = """
+using namespace std;
+
+const int jrll[12] = { 2,2,2,2,3,3,3,3,4,4,4,4 };
+const int jpll[12] = { 1,3,5,7,0,2,4,6,1,3,5,7 };
+
+int num_pixels = n_p;
+for( unsigned int ppp=0; ppp<num_pixels; ppp++ ){
+    
+    int pix = pixs[ppp];
+    int ix, iy, face_num;
+    int nside_in;
+    {
+    int iring, iphi, kshift, nr;
+    int nside_;
+    int npface_, ncap_, npix_;
+    double fact1_, fact2_;
         
+    nside_  = 1<<order_in;
+    npface_ = nside_*nside_;
+    ncap_   = (npface_-nside_)<<1;
+    npix_   = 12*npface_;
+    fact2_  = 4./npix_;
+    fact1_  = (nside_<<1)*fact2_;;
+    int nl2 = 2*nside_;
+    
+    nside_in = nside_;
+    
+if (pix<ncap_) // North Polar cap
+  {
+  iring = int(0.5*(1+int(sqrt(1+2*pix+0.5)))); //counted from North pole
+  iphi  = (pix+1) - 2*iring*(iring-1);
+  kshift = 0;
+  nr = iring;
+  face_num=0;
+  int tmp = iphi-1;
+  if (tmp>=(2*iring))
+    {
+    face_num=2;
+    tmp-=2*iring;
+    }
+  if (tmp>=iring) ++face_num;
+  }
+else if (pix<(npix_-ncap_)) // Equatorial region
+  {
+  int ip = pix - ncap_;
+  if (order_in>=0)
+    {
+    iring = (ip>>(order_in+2)) + nside_; // counted from North pole
+    iphi  = (ip&(4*nside_-1)) + 1;
+    }
+  else
+    {
+    iring = (ip/(4*nside_)) + nside_; // counted from North pole
+    iphi  = (ip%(4*nside_)) + 1;
+    }
+  kshift = (iring+nside_)&1;
+  nr = nside_;
+  unsigned int ire = iring-nside_+1;
+  unsigned int irm = nl2+2-ire;
+  int ifm, ifp;
+  if (order_in>=0)
+    {
+    ifm = (iphi - ire/2 + nside_ -1) >> order_in;
+    ifp = (iphi - irm/2 + nside_ -1) >> order_in;
+    }
+  else
+    {
+    ifm = (iphi - ire/2 + nside_ -1) / nside_;
+    ifp = (iphi - irm/2 + nside_ -1) / nside_;
+    }
+  if (ifp == ifm) // faces 4 to 7
+    face_num = (ifp==4) ? 4 : ifp+4;
+  else if (ifp<ifm) // (half-)faces 0 to 3
+    face_num = ifp;
+  else // (half-)faces 8 to 11
+    face_num = ifm + 8;
+  }
+else // South Polar cap
+  {
+  int ip = npix_ - pix;
+  iring = int(0.5*(1+int(sqrt(2*ip-1+0.5)))); //counted from South pole
+  iphi  = 4*iring + 1 - (ip - 2*iring*(iring-1));
+  kshift = 0;
+  nr = iring;
+  iring = 2*nl2-iring;
+  face_num=8;
+  int tmp = iphi-1;
+  if (tmp>=(2*nr))
+    {
+    face_num=10;
+    tmp-=2*nr;
+    }
+  if (tmp>=nr) ++face_num;
+  }
+
+    int irt = iring - (jrll[face_num]*nside_) + 1;
+    int ipt = 2*iphi- jpll[face_num]*nr - kshift -1;
+    if (ipt>=nl2) ipt-=8*nside_;
+
+    ix =  (ipt-irt) >>1;
+    iy =(-(ipt+irt))>>1;
+
+    }
+    // end ring_to_xyf, start xyf_to_ring
+    {
+    int nside_;
+    int npface_, ncap_, npix_;
+    nside_  = 1<<order_out;
+    
+    npface_ = nside_*nside_;
+    ncap_   = (npface_-nside_)<<1;
+    npix_   = 12*npface_;
+    int nl4 = 4*nside_;
+    
+    // degrade!!
+    float fact=nside_in/nside_;
+    ix = int(ix/fact);
+    iy = int(iy/fact);
+    
+    int jr = (jrll[face_num]*nside_) - ix - iy  - 1;
+    
+    int nr, kshift, n_before;
+    if (jr<nside_)
+      {
+      nr = jr;
+      n_before = 2*nr*(nr-1);
+      kshift = 0;
+      }
+    else if (jr > 3*nside_)
+      {
+      nr = nl4-jr;
+      n_before = npix_ - 2*(nr+1)*nr;
+      kshift = 0;
+      }
+    else
+      {
+      nr = nside_;
+      n_before = ncap_ + (jr-nside_)*nl4;
+      kshift = (jr-nside_)&1;
+      }
+
+    int jp = (jpll[face_num]*nr + ix - iy + 1 + kshift) / 2;
+    if (jp>nl4)
+      jp-=nl4;
+    else
+      if (jp<1) jp+=nl4;
+
+    pixs[ppp] = n_before + jp - 1;
+    }
+    }
+
+"""
+
+def degrade( map_in, order_out ):
+    
+    assert map_in.scheme == 0, "Error, partpix_map:degrade only defined for scheme=0, not {0}".format(map_in.scheme)
+    
+    pixs = list(map_in.pixel_mapping_arraytohigh.copy())
+    order_in = int(map_in.order)
+    order_out = int(order_out)
+    n_p = len(pixs)
+    new_pixs = list(np.zeros(n_p))
+    args = ['pixs', 'n_p', 'order_in', 'order_out'],
+    # print "before: %d %d %d %d" %(len(pixs), pixs[0], pixs[1], pixs[-1])
+    weave.inline(code_degrade_rings, *args, type_converters=converters.blitz)
+    # print "after: %d %d %d %d" %(len(pixs), pixs[0], pixs[1], pixs[-1])
+    lrpixs = pixs
+    
+    map_out = Partpix_Map()
+    unique_pixels, unique_indices = np.unique(lrpixs, return_inverse=True)
+    nu = len(unique_pixels)
+    map_out.pixel_mapping_arraytohigh = unique_pixels
+    map_out.partmap = np.zeros(nu)
+    for p in range(len(lrpixs)):
+        map_out.partmap[unique_indices[p]] += map_in.partmap[p]
+        
+    map_out.order = order_out
+    map_out.update()
+    fact = map_in.Nside()/map_out.Nside()
+    map_out.partmap /= (fact*fact)
+    
+    # hpmap = map_out.to_healpix()
+    # healpy.fitsfunc.write_map("temp.fits",hpmap)
+    
+    return map_out
